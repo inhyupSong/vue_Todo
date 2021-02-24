ac976d588e8ffda57822b706fd346e794eea6625
InhyupSong <inhyup.song@bint.ch>
2021-02-24 20:37:54 +0100

    build baisc Structure with tutorial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
     <h1>Welcome home</h1>
+    <h2>{{ name }}</h2>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: 'Funny Vue-coding',
+    };
+  },
+};
 </script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
