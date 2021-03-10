c761af048528393078d0f9a79531bdf89559a52f
InhyupSong <inhyup.song@bint.ch>
2021-03-10 15:16:45 +0100

    update Code for Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,59 @@
 <template>
-  <div id="app">
-    <TopMenu />
-    <div id="content" class="content">
-      <div>
-        <!--    <router-link to="/">Home </router-link>
-        <router-link to="/about"> About </router-link> -->
-        <router-view />
-      </div>
-    </div>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <CompletedTodo />
+    <AddTodo @add-todo="addTodo"/>
+    <hr>
+    <TodoList
+      @toggle-checkbox="toggleCheckbox"
+      @click-delete="deleteTodo"
+    />
   </div>
 </template>
 
 <script>
-import TopMenu from './components/layout/TopMenu.vue'
+import TodoList from '@/components/TodoList';
+import AddTodo from '@/components/AddTodo';
+import CompletedTodo from '@/components/CompletedTodo';
 
 export default {
-  name: 'app',
   components: {
-    TopMenu,
+    TodoList,
+    AddTodo,
+    CompletedTodo,
   },
+  data() {
+    return {
+      todoText: '',
+      // todos: [
+      //   { id: 1, text: 'buy a car', checked: false},
+      //   { id: 2, text: 'play game', checked: false},
+      // ]
+    }
+  },
+
+  methods: {
+    deleteTodo(id) {
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      });
+      this.todos.splice(index, 1);
+      // this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(value) {
+      this.todos.push({
+        id: Math.random(),
+        text: value,
+        checked: false
+      });
+      this.todoText = '';
+    },
+    toggleCheckbox({id, checked}) {
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      });
+      this.todos[index].checked = checked;
+    }
+  }
 }
 </script>
-
-<style></style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,10 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
 
-// Install BootstrapVue
-Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin);
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  router,
+  store,
   render: h => h(App),
-}).$mount('#app');
+}).$mount('#app')
